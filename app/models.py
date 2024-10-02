4b8f3b5d437a94463d94d4a58b9f8a002dcf438d
ksg-dev <ksg.dev.data@gmail.com>
2024-10-02 15:52:01 -0500

    add base for resource library html

--- app/models.py
+++ app/models.py
@@ -122,6 +122,7 @@ class Resource(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(250), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
+    type: Mapped[str] = mapped_column(String(100), nullable=True)
     resource_url: Mapped[str] = mapped_column(String(250), nullable=True)
 
     # Many-to-many relationship to concepts
