ccef25b231bb9e3e50d2256418759cc07eb0e3cb
ksg-dev <ksg.dev.data@gmail.com>
2024-10-29 14:06:46 -0500

    take csv import for courses and add to db

--- app/import.py
+++ app/import.py
@@ -16,36 +16,40 @@ def import_courses(filename, user_id):
         'content_hours': float,
         'has_cert': bool
     }
-    data = pd.read_csv(f"imports/{filename}", dtype=col_types)
+    data = pd.read_csv(f"imports/{filename}", dtype=col_types, index_col=False, header=0)
 
-    for course in data:
-        # new_course = Course(
-        #     name=course["name"],
-        #     platform=course["platform"],
-        #     url=course["url"],
-        #     instructor=course["instructor"],
-        #     start=pd.to_datetime(course["start"]),
-        #     complete=pd.to_datetime(course["complete"]),
-        #     content_hours=course["content_hours"],
-        #     has_cert=course["has_cert"],
-        #     date_added=date.today(),
-        #     user_id=user_id
-        # )
+    for row in data.itertuples(index=False):
+        # print(row.name)
+        # print(row.platform)
+        new_course = Course(
+            name=row.name,
+            platform=row.platform,
+            url=row.url,
+            instructor=row.instructor,
+            start=pd.to_datetime(row.start),
+            complete=pd.to_datetime(row.complete),
+            content_hours=row.content_hours,
+            has_cert=row.has_cert,
+            date_added=date.today(),
+            user_id=user_id
+        )
 
-        # if course["complete"]:
-        #     new_course.status = 'complete'
-        #
-        # elif course["start"]:
-        #     new_course.status = 'in-progress'
-        #
-        # else:
-        #     new_course.status = 'complete'
+        if row.complete:
+            new_course.status = 'complete'
 
-        # db.session.add(new_course)
-        # db.session.commit()
+        elif row.start:
+            new_course.status = 'in-progress'
 
-    # response_msg = "Course Import Successful"
-    #     print(new_course)
+        else:
+            new_course.status = 'complete'
+
+        # print(new_course.name)
+        db.session.add(new_course)
+        db.session.commit()
+    # print(data.info())
+
+    response_msg = "Course Import Successful"
+
 
     # name = data["name"]
     # start = data["start"]
@@ -56,7 +60,7 @@ def import_courses(filename, user_id):
     # print(f"NAME: {start} TYPE: {type(start)}")
     # print(f"NAME: {has_cert} TYPE: {type(has_cert)}")
 
-    # print(response_msg)
+    print(response_msg)
 
 
 import_courses("test_courses.csv", user_id=1)
