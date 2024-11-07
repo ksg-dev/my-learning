5a8f0163904acc6904c1dc3ba755e8541436cf8d
ksg-dev <ksg.dev.data@gmail.com>
2024-11-07 13:39:59 -0600

    project and library param dicts

---
+++ app/upload_dicts.py
@@ -0,0 +1,118 @@
+course_params = [
+    {
+        'col': 1,
+        'name': 'name',
+        'input': 'string'
+    },
+    {
+        'col': 2,
+        'name': 'platform',
+        'input': 'string'
+    },
+    {
+        'col': 3,
+        'name': 'url',
+        'input': 'url string'
+    },
+    {
+        'col': 4,
+        'name': 'instructor',
+        'input': 'string'
+    },
+    {
+        'col': 5,
+        'name': 'start',
+        'input': 'date m/dd/YYYY'
+    },
+    {
+        'col': 6,
+        'name': 'complete',
+        'input': 'date m/dd/YYYY'
+    },
+    {
+        'col': 7,
+        'name': 'content_hours',
+        'input': 'float'
+    },
+    {
+        'col': 8,
+        'name': 'has_cert',
+        'input': 'True/False case insensitive'
+    },
+]
+
+project_params = [
+    {
+        'col': 1,
+        'name': 'name',
+        'input': 'string'
+    },
+    {
+        'col': 2,
+        'name': 'description',
+        'input': 'string'
+    },
+    {
+        'col': 3,
+        'name': 'assignment_link',
+        'input': 'url string'
+    },
+    {
+        'col': 4,
+        'name': 'start',
+        'input': 'date m/dd/YYYY'
+    },
+    {
+        'col': 5,
+        'name': 'complete',
+        'input': 'date m/dd/YYYY'
+    },
+    {
+        'col': 6,
+        'name': 'section',
+        'input': 'string'
+    },
+    {
+        'col': 7,
+        'name': 'lecture',
+        'input': 'string'
+    },
+    {
+        'col': 8,
+        'name': 'repo',
+        'input': 'string matching existing repo name'
+    },
+    {
+        'col': 9,
+        'name': 'concepts',
+        'input': 'string separated by +, ex: "tkinter+gui+try block+random"'
+    },
+    {
+        'col': 10,
+        'name': 'course',
+        'input': 'string matching existing course name'
+    },
+]
+
+library_params = [
+    {
+        'col': 1,
+        'name': 'name',
+        'input': 'string'
+    },
+    {
+        'col': 2,
+        'name': 'description',
+        'input': 'string'
+    },
+    {
+        'col': 3,
+        'name': 'doc_link',
+        'input': 'url string to reference docs'
+    },
+    {
+        'col': 4,
+        'name': 'concepts',
+        'input': 'string separated by +, ex: "tkinter+gui+try block+random"'
+    },
+]
