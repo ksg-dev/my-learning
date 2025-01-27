d47d5607d4a862dca9ced02d8690b0352938375d
ksg-dev <ksg.dev.data@gmail.com>
2025-01-27 13:22:25 -0600

    smtphandler for error logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config, Base
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 
@@ -20,5 +22,35 @@ login_manager.login_view = 'login'
 
 # db migration engine object
 migrate = Migrate(app, db, render_as_batch=True)
+
+
+# Log errors by email
+# If app debug false
+if not app.debug:
+    # if mail server in config
+    if app.config['MAIL_SERVER']:
+        # Establish auth variable and set to None
+        auth = None
+        # If email username or password in config
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            # auth tuple updated with username and password
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        # Est secure variable and set to None
+        secure = None
+        # If mail use tls is not none, set secure to empty tuple for tls secure protocol
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        # create SMTPHandler object
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='My-Learning Failure',
+            credentials=auth, secure=secure
+        )
+        # Set level to report errors and not warnings
+        mail_handler.setLevel(logging.ERROR)
+        # Attach handler to app.logger object from Flask
+        app.logger.addHandler(mail_handler)
+
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -10,6 +10,13 @@ class Config:
     SECRET_KEY = os.environ["SECRET_KEY"]
     SQLALCHEMY_DATABASE_URI = os.environ["DB_URI"]
 
+    MAIL_SERVER = os.environ["MAIL_SERVER"]
+    MAIL_PORT = int(os.environ["MAIL_PORT"])
+    MAIL_USE_TLS = os.environ["MAIL_USE_TLS"] is not None
+    MAIL_USERNAME = os.environ["MAIL_USERNAME"]
+    MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
+    ADMINS = ['testemail.ksg.data@gmail.com']
+
 
 class Base(DeclarativeBase):
     pass
